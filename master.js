bf89eec0b624ef0356f313cc0a16c5d691753f62
Svegin <svegin@Svegins-Mac-mini.local>
2016-12-28 18:03:37 -0600

    array.some()

--- master.js
+++ master.js
@@ -17,6 +17,16 @@ const comments = [
 
 // Some and Every Checks
 // Array.prototype.some() // is at least one person 19?
+// const isAdult = people.some(function (person) {
+//   const currentYear = (new Date()).getFullYear();
+//   if (currentYear - person.year >= 19) {
+//     return true;
+//   }
+// });
+const isAdult = people.some(person => ((new Date()).getFullYear()) - person.year >= 19);
+
+console.log({isAdult});
+
 // Array.prototype.every() // is everyone 19?
 
 // Array.prototype.find()
